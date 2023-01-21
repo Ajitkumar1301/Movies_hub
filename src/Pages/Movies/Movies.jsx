b4d19516c88c6611d886153e2173ed750fe81cda
ajit kumar <ajit@regami.solutions>
2023-01-21 20:25:09 +0530

    #movies pagination added

--- src/Pages/Movies/Movies.jsx
+++ src/Pages/Movies/Movies.jsx
@@ -1,21 +1,25 @@
 import {useState,useEffect} from 'react'
 import axios from 'axios';
 import SingleContent from '../../components/SingleContent/SingleContent';
+import CustomPagination from '../../components/CustomPagenations/CustomPagination';
 
 const Movies = () => {
   const[content,setContent]=useState([]);
+  const[page,setPage]=useState(1)
+const[numofPages,setNumofPages]=useState();
 
   const fetchTrending=async()=>{
-      const {data}=await axios.get(`https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_API_KEY}`);
+      const {data}=await axios.get(`https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_API_KEY}&page=${page}`);
       // console.log(data);
       setContent(data.results);
-
+      setNumofPages(data.total_pages)
   }    
-
-
- useEffect(()=>{
+ 
+ useEffect(() => {
+  window.scroll(0, 0);
   fetchTrending();
- },[]); 
+}, [page]);
+
 
  return (
   <>
@@ -29,7 +33,8 @@ const Movies = () => {
        date={c.first_air_date || c.release_date}
        media_type={c.media_type}
        vote_average={c.vote_average}/>)
-}</div>    
+}</div> 
+<CustomPagination setPage={setPage} numofPages={numofPages} />   
   </>
 )
 }
